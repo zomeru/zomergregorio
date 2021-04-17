6814143f6d5fe5cf2367f91c7f288c7bb813a7cf
Zomer Gregorio <zomerzxc.19@gmail.com>
2021-04-18 03:30:57 +0800

    first commit

---
+++ script/script.js
@@ -0,0 +1,45 @@
+let intro = document.querySelector('.intro');
+let logo = document.querySelector('.logo-header');
+let logoSpan = document.querySelectorAll('.logo');
+
+window.addEventListener('DOMContentLoaded', ()=>{
+
+    setTimeout(()=>{
+
+        logoSpan.forEach((span, idx)=>{
+            setTimeout(()=>{
+                span.classList.add('active');
+            }, (idx + 1 ) * 400)
+        });
+
+        setTimeout(()=>{
+            logoSpan.forEach((span, idx)=>{
+
+                setTimeout(()=>{
+                    span.classList.remove('active');
+                    span.classList.add('fade');
+                }, (idx + 1) * 50)
+            })
+        }, 2000)
+
+        setTimeout(()=>{
+            intro.style.top = '-100vh';
+        }, 2300)
+
+
+    })
+})
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("nav").style.top = "0";
+  } else {
+    document.getElementById("nav").style.top = "-100%";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+
+
